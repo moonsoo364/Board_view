77d0918af2a870c9f50fc7f1a6624e68c92706a3
moonsoo <aptx15hibr@naver.com>
2022-08-09 10:03:05 +0900

    0809axios 모듈 정리

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,110 +1,91 @@
 <template>
 <div>
     
-<b-navbar toggleable="lg" type="dark" variant="info">
+<b-navbar style="font-size: 1.2rem;" toggleable="lg" type="dark" variant="info">
     
-    
-    <router-link to="/" style="text-decoration:none; margin-left: 5px;"><b-navbar-brand>Home</b-navbar-brand></router-link>
+   
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-    <b-collapse v-if="loginState" id="nav-collapse" is-nav>
-       <b-navbar-nav>
-        <router-link to="/create"><span class="nav-link">글쓰기</span></router-link>
-      </b-navbar-nav>
-      <b-navbar-nav>
-        <b-nav-item class="nav-link" @click="doLogout">로그아웃</b-nav-item>
-      </b-navbar-nav>
-      <b-navbar-nav v-if="getAdmin">
-        <router-link to="/userinfo"><span class="nav-link">관리자</span></router-link>
-      </b-navbar-nav>
-          <b-avatar style="color:rgba(255, 255, 255,0.8)"></b-avatar>
-          <span style="color:rgba(255, 255, 255,0.8)">{{getUsername}}님 반갑습니다!</span>
+    <b-collapse v-if="getLoginState" id="nav-collapse" is-nav>
+
+      <div class="login_nav"><router-link to="/">Home</router-link></div>
+      <div class="login_nav"><router-link to="/create">글쓰기</router-link></div>
+      <div style="cursor:pointer;" class="login_nav" @click="doLogout">로그아웃</div>
+      <div class="login_nav"><router-link v-if="getAdmin" to="/userinfo">관리자</router-link></div>
+      <div style="color:white;" class="login_nav">
+        <b-icon icon="person-fill"></b-icon>
+        {{getUserData.username}}님 반갑습니다!
+      </div>
+      
+      
+    
     </b-collapse>
 
     <b-collapse v-else id="nav-collapse"  is-nav>
-      <b-navbar-nav>
+
         <b-nav-item v-for="item in navbars" :key="item.id">
-          <router-link :to="{path:item.router}" class="active">{{item.menu}}</router-link>
+          <router-link :to="{path:item.router}">{{item.menu}}</router-link>
         </b-nav-item>
-      </b-navbar-nav>
+
     </b-collapse>
     
 </b-navbar>
-
 </div>
 </template>
 <script>
-import axios from 'axios'
 import { mapGetters } from 'vuex';
-
-
+import { getToken } from '@/store/module/token.module';
+import { authAxios } from '@/store/module/axios.module';
 export default {
   name:"NavBar", 
-
-  
- mounted(){
-    
-    const store =this.$store;
+  created(){
+    authAxios.defaults.headers.common['Authorization']=getToken(); 
     if(localStorage.getItem('user')!=null){
-    const token =JSON.parse(localStorage.getItem('user')).token
-    const instance =axios.create({
-      headers:{Authorization:token}
-    })
-    instance.post("/api/auth/expiredCheckToken")
-    .then(function(res){
-      store.dispatch('asyncStorage',{
-        expiredTime:res.data.restedTime,
-        expired:res.data.validated
-      });
-    })
-    .catch(function(err){
-       console.log(err);
-       store.commit('logout');         
-    })
-    }
-
-    
- },
- 
-  computed:{
-    ...mapGetters(['getAdmin']),
-    loginState(){
-      return this.$store.getters.getLoginState;
-    },
-    getUsername(){
-      return this.$store.getters.getUsername;
+      this.$store.dispatch('checkUserToken');
     }
   },
-      data(){
-            return{
-                hover:false,
-                compute:0,
-                navbars:[
-                  {menu: '로그인', router:'/login',id:1},
-                  {menu: '회원가입', router:'/join',id:2},
-                ],
-               
-            }
-        },
-      methods:{
-       
-        doLogout:function(){
-          this.$store.commit('logout')
-        },
-      }
+  updated(){
+    authAxios.defaults.headers.common['Authorization']=getToken();
+  },
+  computed:{
+   
+   ...mapGetters(['getAdmin','getUserData','getLoginState']),
+   
+  },
+  data(){
+    return{
+        navbars:[
+          {menu: 'Home', router:'/',id:1},
+          {menu: '로그인', router:'/login',id:2},
+          {menu: '회원가입', router:'/join',id:3},
+        ],   
+    }
+    },
+  methods:{
+    doLogout:function(){
+      this.$store.commit('logout')
+    },
+  }
 }
 </script>
 <style scoped>
-a{
-  text-decoration: none;
-  color: rgba(255,255,255,0.8);
-}
-.active:hover{
-  font-weight: bold;
-}
-.navbar-brand, .nav-link{
-  font-size: 1.5rem;
+  a{
+    text-decoration: none;
+    color:white;
+  }
+  li{
+    list-style: none;
+  }
+  #nav-collapse{
+    padding-top: 0.5rem;
+    padding-bottom: 0.5rem;
+  }
+  .login_nav{
+    padding: 0.5rem 1rem;
+    color: white;
+  }
+  .login_nav:hover{
+    color: #0a58ca;
+  }
 
-}
-
-
+  
 </style>
---
+++ src/store/module/userStore.js
@@ -0,0 +1,248 @@
+import router from "@/router";
+import {noAuthAxios,authAxios} from "@/store/module/axios.module";
+import moment from "moment";
+
+export const userStore=({
+
+  state: {
+    userData: {token:'',username:'',expiredTime:0,isAdmin:false,id:0},//로그인 할때 ls저장
+    isLogin: false,
+    noOverlapId:false,
+    isExitedPw:false,
+    userList:[],
+  },
+  getters:{
+   
+    getLoginState:function(state){
+      //*NavBar
+      //용도: 새로고침할 때도 사용자 정보 유지
+      if(localStorage.getItem('user')==null){
+        state.isLogin=false;
+        return state.isLogin;
+      }else{
+        state.isLogin=true;
+        return state.isLogin;
+      }
+      
+    },
+    setLocationStorage:function(state){
+      localStorage.removeItem('user');
+      state.isLogin=false;
+      state.userData.token='';
+      state.userData.username='';
+
+    },
+    getUserData:function(state){
+
+      return state.userData;
+    },
+
+    getAdmin:function(state){
+      return state.userData.isAdmin;
+    }
+    ,
+    getUserList:function(state){
+      return state.userList;
+    },
+    getNoOverLapId:function(state){
+      return state.noOverlapId;
+    },
+    getIsExistedPw:function(state){
+      return state.isExitedPw;
+    }
+
+  },
+  mutations:{
+
+    
+    setUserData:function(state,payload){
+      //로그인 후 동작 *LoginForm
+      localStorage.removeItem('user');
+      state.userData.username=payload.username;
+      state.userData.token=payload.token;
+      state.userData.expiredTime=payload.expiredTime;
+      state.userData.isAdmin=payload.admin;
+      state.userData.id=payload.id;
+      state.isLogin=true// *NavBar 로그인 상태 토클
+
+      localStorage.setItem('user',JSON.stringify(state.userData));
+    },
+    logout:function(state){
+      // *LoginForm, *NavBar
+      // 1. 토큰 시간 지나면 로그아웃 *LoginForm
+      // 2. 로그아웃 버튼 누르면 로그아웃
+      localStorage.removeItem('user');
+      state.userData.token='';
+      state.userData.username='';
+      state.userData.isAdmin=false;
+      state.isLogin=false;
+      
+
+      alert("로그아웃 되었습니다!")
+      router.push('/')
+       
+    },
+    getStorage:function(state,payload){
+      state.userData.username=payload.username;
+      state.userData.token=payload.token;
+      state.userData.isAdmin=payload.isAdmin;
+      state.isLogin=true
+      localStorage.removeItem('user');
+      localStorage.setItem('user',JSON.stringify(state.userData));
+
+    },
+
+    setUserList:function(state){
+
+      authAxios.post('/admin/all_user_infomation')
+      .then((res)=>{console.log(res);
+        state.userList=res.data;
+        for(let i=0;i<state.userList.length;i++){
+          
+          state.userList[i].createDate =moment(state.userList[i].createDate).format("yyyy/MM/DD h:mm:ss a");
+        }
+        console.log(state.userList);
+      })
+      .catch((err)=>{
+        console.log(err);
+      })
+      
+     
+      
+    },
+    overlapCheckId:function(state,payload){
+      if(payload){
+        noAuthAxios.post('/name_check',{
+          username:payload
+        }).then(function(res){
+          if(res.data){
+            alert("사용가능한 아이디입니다.");
+            state.noOverlapId=true;
+          }else{
+            alert("중복된 아이디입니다.");
+          }
+        }).catch((err)=>{
+          console.log(err);
+          alert("아이디확인 불가!")
+        })
+
+      }else{
+        alert("아이디를 먼저 입력하세요!")
+      }
+    },
+    inverseOverlapId:function(state){
+      state.noOverlapId = !state.noOverlapId;
+    },
+    existedPassword:function(state,payload){
+      authAxios.post('/admin/password_existing',{
+        password:payload.password,id:payload.id})
+        .then((res)=>{
+            console.log(res.data);
+            if(res.data){
+              alert("이미 사용하고 있는 비밀번호입니다.");
+              state.isExitedPw=false;
+
+            }else{
+              alert("사용가능한 비밀번호입니다.");
+             
+              state.isExitedPw=true;
+            }
+        }).catch(function(err){
+            alert("JWT이 제대로인식이 안됩니다!")
+            console.log(err);
+
+        })
+    },
+    updateUser:function(state,payload){
+      authAxios.post('/admin/user_update',{
+        password:payload.password,
+        email:payload.email,
+        id:payload.id
+      }).then(function(res){  
+        console.log(res);
+        alert("회원 정보 수정완료!")
+        router.push("/userinfo");
+    }).catch((e)=>{
+          console.log(e);
+          alert("회원가입 실패 !");
+    })
+    },
+    togleExitedPw:function(state,){
+      state.isExitedPw=!state.isExitedPw;
+      console.log(state.isExitedPw);
+    },
+    },
+    
+  actions:{
+    userJoin: function(state,payload){
+      noAuthAxios.post('/user_join',{
+        username:payload.username,
+        password:payload.password,
+        email:payload.email,
+        roles:payload.role
+      }).then(function(res){
+        console.log(res.data);
+        console.log(authAxios);
+        alert("회원가입 성공 !")
+        router.push('/')
+      }).catch((err)=>{
+         console.log(err);
+         alert("회원가입 실패 !")
+
+      })
+    },
+    loginUser:function(state,payload){
+      noAuthAxios.post('/user_login',{
+        username:payload.username,
+        password:payload.password,
+      }).then((res)=>{
+        alert("로그인 성공!");
+        router.push('/');
+        console.log(res);
+        state.commit('setUserData',{
+          username:payload.username,
+          token:res.data.token,
+          expiredTime:res.data.expiredTime,
+          admin:res.data.admin,
+          id:res.data.id
+        });
+        setTimeout(function(){state.commit('logout')},res.data.expiredTime);
+      }).catch(err=>{
+         console.log(err);
+        if(err.response.data.code==1){
+          alert("비밀번호가 일치하지 않습니다!")
+        }else if(err.response.data.code==2){
+          alert("존재하지 않는 아이디 입니다!")
+        }
+      })    
+    },
+    checkUserToken:function(state){
+      const username=JSON.parse(localStorage.getItem('user')).username;
+      const token=JSON.parse(localStorage.getItem('user')).token;
+      const isAdmin=JSON.parse(localStorage.getItem('user')).isAdmin;
+
+      authAxios.post("/auth/token_check")
+      .then(function(res){
+        console.log(res);
+        if(res.data.validated){
+          state.commit('getStorage',{
+            restedTime:res.data.restedTime,
+            username:username,
+            token:token,
+            isAdmin:isAdmin
+          });
+          setTimeout(function(){state.commit('logout')},res.data.restedTime);
+        }else {
+          state.commit('logout');
+        }
+
+      })
+      .catch(function(err){
+         console.log(err);
+         state.commit('logout');         
+      })
+      }
+  },
+  
+
+})
